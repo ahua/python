e7c8ca3c77ada724ae33b92a9d64a3923ec83c38
yhyan <yhyan@geek.(none)>
2013-03-17 20:27:02 +0800

    a

--- recipes/os/filerename.py
+++ recipes/os/filerename.py
@@ -2,15 +2,25 @@
 
 import sys
 import os
+import re
 
-
-def get_new_filename(filename):
+def less3d(filename):
     i = filename.rfind(".")
     suffix = filename[i:]
     name = filename[:i]
 
     new_name = "%03d" % int(name.replace("lesson","").strip(" "))
     return new_name + suffix
+
+def friends(filename):
+    i = filename.rfind(".")
+    suffix = filename[i:]
+    new_name = re.search("S\d\dE\d\d(\-\d\d)?", filename).group(0)
+    return new_name + suffix
+
+
+def get_new_filename(filename):
+    return friends(filename)
 
 def help():
     usage = "$ prog dir"
@@ -23,7 +33,7 @@ if __name__ == "__main__":
     filenames = os.listdir(d)
     for filename in filenames:
         new_filename = get_new_filename(filename)
-        print new_filename
-        os.rename(filename, new_filename)
+        print filename, " --> ", new_filename
+        #os.rename(filename, new_filename)
         
 
