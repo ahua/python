219fef57c4949368298eb73704bfcd86a14066f6
yhyan <yhyan@geek.(none)>
2013-05-04 17:59:58 +0800

    add openstack-dashboard.

---
+++ openstack-dashboard/manage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+sys.path.append("/usr/share/openstack-dashboard")
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openstack_dashboard.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
